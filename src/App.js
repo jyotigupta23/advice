1f6619e9d1e105d80ed8c8891f5303b24c7d258b
jyotigupta23 <jyotigupta141place@gmail.com>
2022-08-27 15:35:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+    state = { advice:''}; //value!
 
-export default App;
+    componentDidMount(){
+        // axios.get(); //since required many times, will create a seprate funtion fetchAdvice
+        // console.log('COMPONENT DID MOUNT')
+        this.fetchAdvice();
+    }
+    fetchAdvice = ()=>{
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response)=>{
+            // console.log(response.data.slip.advice);
+
+            const {advice} = response.data.slip;
+            this.setState({advice}); //when property name is equal to value name we prefer using once.
+
+
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+    render(){
+        const {advice} = this.state;
+        return(
+            // <h1>{advice}</h1> //<h1>{this.state.advice}</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>Give Me Advice</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}export default App;
